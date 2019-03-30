b5e2af3b28fe1d3cc6902bf17d3e131c5cb6d566
Eric Gonzalez <gonzerc@users.noreply.github.com>
2019-03-29 23:32:08 -0700

    altered move script on agents

--- NavMeshTutorial/Assets/Move.cs
+++ NavMeshTutorial/Assets/Move.cs
@@ -4,62 +4,31 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Move : MonoBehaviour {
-    public Transform waypoint1;
-    public Transform waypoint2;
-    public Transform waypoint3;
-    NavMeshAgent agent;
 
-    private bool visited1;
-    private bool visited2;
-    private bool visited3;
+    public List<Transform> Waypoints;
+
+    private NavMeshAgent agent;
+    private Transform currentWaypoint;
 
 	// Use this for initialization
 	void Start () {
-        transform.position = new Vector3(0,1,-10);
         agent = GetComponent<NavMeshAgent>();
-
-        visited1 = true;
-        visited2 = false;
-        visited3 = true;
+        SetNewDestinaton();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (!visited1)
-        {
-            // visit waypoint1's position
-            agent.SetDestination(waypoint1.position);
 
-            if (waypoint1.position.x == agent.transform.position.x && waypoint1.position.z == agent.transform.position.z)
-            {
-                visited1 = true;
-                visited2 = false;
-                visited3 = true;
-            }
-        }
-        if (!visited2)
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - currentWaypoint.position).magnitude <= 1)
         {
-            // visit waypoint2's position
-            agent.SetDestination(waypoint2.position);
-
-            if (waypoint2.position.x == agent.transform.position.x && waypoint2.position.z == agent.transform.position.z)
-            {
-                visited1 = true;
-                visited2 = true;
-                visited3 = false;
-            }
+            Debug.Log("New Waypoint: " + currentWaypoint.position);
+            SetNewDestinaton();
         }
-        if (!visited3)
-        {
-            // visit waypoint2's position
-            agent.SetDestination(waypoint3.position);
+    }
 
-            if (waypoint3.position.x == agent.transform.position.x && waypoint3.position.z == agent.transform.position.z)
-            {
-                visited1 = false;
-                visited2 = true;
-                visited3 = true;
-            }
-        }
+    void SetNewDestinaton()
+    {
+        currentWaypoint = Waypoints[Random.Range(0, Waypoints.Count)];
+        agent.SetDestination(currentWaypoint.position);
     }
 }
